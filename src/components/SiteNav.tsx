e87b74bdcb82e75c9be6604fa35f8ebde52c7412
Saurabh chavan <manishff87@gmail.com>
2026-09-16 19:33:00 +0530

    feat: add shared cinematic navigation

---
+++ src/components/SiteNav.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export function SiteNav() {
+  return <header className="site-nav"><nav className="nav-inner"><a className="brand" href="/">SAURABH<span>.</span></a><div className="nav-links"><a href="/work">WORK</a><a href="/path">PATH</a><a href="/capabilities">CAPABILITIES</a><a href="/contact">CONTACT</a></div><a className="nav-button" href="/contact">LET&apos;S TALK</a></nav></header>
+}
